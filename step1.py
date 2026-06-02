3ffe735c459b779f8daf7766a71c6e2d510a69ac
williamsDambes <williamsdambes2@gmail.com>
2026-06-02 15:30:22 +0300

    Improve comments for wav file usage
    Updated comments to guide users on using a real .wav file.

--- step1.py
+++ step1.py
@@ -3,8 +3,6 @@ from scipy.io import wavfile
 
 # This function opens a .wav file
 def start_here():
-    # Replace 'test.wav' with the name of a real wav file in your folder!
-    # You can download a small sample wav online.
     try:
         sampling_rate, data = wavfile.read('test.wav')
         
